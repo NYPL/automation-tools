6f3f66c5158ec76f3e9dfd2b714914c31126cc66
nkrabben <n@nkrabben.com>
2017-11-20 21:32:25 -0500

    remove sample configurations

--- etc/transfer-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# transfer script example
-# /etc/archivematica/automation-tools/transfer-script.sh
-cd /usr/lib/archivematica/automation-tools/
-/usr/share/python/automation-tools/bin/python -m transfers.transfer --user <user> --api-key <apikey>  --ss-user <user> --ss-api-key <apikey> --transfer-source <transfer_source_uuid> --config-file <config_file>
